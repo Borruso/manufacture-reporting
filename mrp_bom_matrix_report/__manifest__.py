8a5b39ff717b072fa6029a53e05f2cc5dc210f0e
Borruso <gborruso@dinamicheaziendali.it>
2026-03-13 13:59:42 +0100

    [MIG] mrp_bom_matrix_report: Migration to 19.0

--- mrp_bom_matrix_report/__manifest__.py
+++ mrp_bom_matrix_report/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     "name": "MRP BOM Matrix Report",
-    "version": "18.0.1.0.0",
+    "version": "19.0.1.0.0",
     "license": "LGPL-3",
     "author": "ForgeFlow, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/manufacture-reporting",
