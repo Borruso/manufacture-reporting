b7cb98a483d834b909774bfd63eb366d5b21cdbd
Borruso <gborruso@dinamicheaziendali.it>
2026-06-26 16:12:08 +0200

    [MIG] mrp_bom_structure_xlsx_level_1: Migration to 19.0

--- mrp_bom_structure_xlsx_level_1/tests/test_mrp_bom_structure_xlsx_level_1.py
+++ mrp_bom_structure_xlsx_level_1/tests/test_mrp_bom_structure_xlsx_level_1.py
@@ -1,7 +1,9 @@
 # Copyright 2024 Tecnativa - Víctor Martínez
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
-from xlrd import open_workbook
+import io
+
+import openpyxl
 
 from odoo.addons.mrp_bom_structure_xlsx.tests.common import TestMrpBomStructureXlsxBase
 
@@ -11,11 +13,9 @@ class TestMrpBomStructureXlsxLevel1(TestMrpBomStructureXlsxBase):
         res = self.report_model._render(
             "mrp_bom_structure_xlsx_l1.bom_structure_xlsx_l1", self.bom.ids, False
         )
-        wb = open_workbook(file_contents=res[0])
-        sheet = wb.sheet_by_index(0)
-        references = []
-        for rownum in range(3, sheet.nrows):
-            references.append(sheet.row_values(rownum)[2])
+        wb = openpyxl.load_workbook(io.BytesIO(res[0]))
+        sheet = wb.active
+        references = [row[2] for row in sheet.iter_rows(min_row=3, values_only=True)]
         self.assertIn("COMPONENT-A", references)
         self.assertIn("COMPONENT-B", references)
         self.assertNotIn("CHILD-COMPONENT", references)
