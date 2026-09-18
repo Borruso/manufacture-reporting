75d0553f3b7d07763b2843c3a5b3733169f13565
Borruso <gborruso@dinamicheaziendali.it>
2026-09-18 10:08:54 +0200

    [MIG] mrp_bom_structure_xlsx_level_1: Migration to 19.0

--- mrp_bom_structure_xlsx_level_1/tests/test_mrp_bom_structure_xlsx_level_1.py
+++ mrp_bom_structure_xlsx_level_1/tests/test_mrp_bom_structure_xlsx_level_1.py
@@ -13,9 +13,7 @@ class TestMrpBomStructureXlsxLevel1(TestMrpBomStructureXlsxBase):
         )
         wb = open_workbook(file_contents=res[0])
         sheet = wb.sheet_by_index(0)
-        references = []
-        for rownum in range(3, sheet.nrows):
-            references.append(sheet.row_values(rownum)[2])
+        references = [sheet.cell_value(row, 2) for row in range(2, sheet.nrows)]
         self.assertIn("COMPONENT-A", references)
         self.assertIn("COMPONENT-B", references)
         self.assertNotIn("CHILD-COMPONENT", references)
